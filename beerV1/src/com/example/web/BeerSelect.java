214e49cdca7efe632d7970c310a478288b90c825
rafamarquesi <marquesi.cc@gmail.com>
2013-07-09 18:33:48 -0400

    estudando (pag. 155)

--- beerV1/src/com/example/web/BeerSelect.java
+++ beerV1/src/com/example/web/BeerSelect.java
@@ -8,23 +8,22 @@ import java.util.*;
 
 public class BeerSelect extends HttpServlet
 {
-   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
-   {
-   
-      String c = request.getParameter("color");
-      String b = request.getParameter("body");
-      
-      BeerExpert especialista = new BeerExpert();
-      List result = especialista.getBrands(c, b);
-      
-      //response.setContentType("text/html");
-      //PrintWriter out = response.getWriter();
-      //out.println("Beer Selection Advice<br>");
-      
-      request.setAttribute("styles", result);
-      
-      RequestDispatcher view = request.getRequestDispatcher("result.jsp");
-      
-      view.forward(request,response);
-   }
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+	{
+		String c = request.getParameter("color");
+		String b = request.getParameter("body");
+
+		BeerExpert especialista = new BeerExpert();
+		List result = especialista.getBrands(c, b);
+
+		//response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();
+		//out.println("Beer Selection Advice<br>");
+
+		request.setAttribute("styles", result);
+
+		RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+
+		view.forward(request,response);
+	}
 }
