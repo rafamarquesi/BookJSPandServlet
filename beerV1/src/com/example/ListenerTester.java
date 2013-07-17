593981c80b74ef2ee85aa9b410fb9575493c37e0
rafamarquesi <marquesi.cc@gmail.com>
2013-07-17 11:30:43 -0400

    estudando pag. (174)

---
+++ beerV1/src/com/example/ListenerTester.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListenerTester extends HttpServlet {
+
+	public ListenerTester() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("test context attributes set by listener<br>");
+		
+		out.println("<br>");
+		
+		Dog dog = (Dog) getServletContext().getAttribute("dog");
+		
+		out.println("Dog's breed is: " + dog.getBreed());
+	}
+}
